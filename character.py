1cd730f19e8886733d179eec51f50ccea5275548
MarkMarritt <123068224+MarkMarritt@users.noreply.github.com>
2023-09-17 17:09:03 +0100

    added to do list

--- character.py
+++ character.py
@@ -1,28 +1,29 @@
 import pygame as pg
 
-def clicked():
-    print("clicked")
-
 class Character(pg.sprite.Sprite):
-    def __init__(self, x, y, action = clicked):
+    def __init__(self, x, y):
         super().__init__()
-        self.type = None
         self.position = (x, y)
+        self.image = pg.Surface([20, 20])
+        self.image.fill("red")
+        self.rect = self.image.get_rect()
+        self.rect.x = self.position[0]
+        self.rect.y = self.position[1]
         self.maxMoves = 3
         self.moves = self.maxMoves
         self.health = 100
         self.damage = 50
         self.attacked = False
-        self.image = pg.Surface([20, 20])
-        self.image.fill("red")
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.action = action
+        self.type = None
     
     def update(self, events):
+        """update character when clicked"""
         for event in events:
             if event.type == pg.MOUSEBUTTONDOWN:
                 if self.rect.collidepoint(event.pos):
-                    self.action()
+                    print("Clicked")
+    
+    def onClicked(self):
+        """method for character when they are clicked"""
+    
 
