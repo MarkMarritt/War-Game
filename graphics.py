3e609abbc9dc33398b3ca4b9b17b225322bf24e3
GABRIELA PYKA <gabriela@pyka.co.uk>
2023-09-17 12:40:26 +0100

    pygame

--- graphics.py
+++ graphics.py
@@ -1,9 +1,8 @@
-import pygames as pg
+import pygame as pg
 
 dWidth = 1200
 dHieght = 720
 
-#Hex grid- 26 x 18; 1 
 background_img = pg.image.scale(pg.image.load("Images/HexGrid.png"), (dWidth, dHieght))
 
 
